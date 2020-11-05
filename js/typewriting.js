a1a8c850a39098bfe624156ae25ae5de772c5afe
ludinhquan <ludinhquan@gmail.com>
2020-11-05 17:37:53 +0700

    Updates

--- js/typewriting.js
+++ js/typewriting.js
@@ -1,19 +1,31 @@
-const jobs = ["Web Developer", "Programmer", "Designer"];
-let index = 0;
-let count = 0;
+function type({
+  words,
+  elementId,
+  index = 0,
+  count = 0,
+  textContent = "",
+  timeCharacterTypingDelay = 80,
+  timeWordTypingDelay = 500,
+}) {
+  let currentJob = count === words.length ? words.join(", ") : words[count];
 
-(function type() {
-  let currentJob = count === jobs.length ? jobs.join(", ") : jobs[count];
-  const textContent = currentJob.slice(0, ++index);
-  document.getElementById("typing").textContent = textContent;
+  textContent += currentJob[index++];
 
+  document.getElementById(elementId).textContent = textContent;
+
+  const delayTypingFn = () =>
+    setTimeout(
+      () => type({ elementId, words, index, count, textContent }),
+      timeCharacterTypingDelay
+    );
+
+  // End of word
   if (index === currentJob.length) {
     index = 0;
-    count = count === jobs.length ? 0 : count + 1;
-    setTimeout(() => {
-      setTimeout(type, 80);
-    }, 500);
+    textContent = "";
+    count = count === words.length ? 0 : count + 1;
+    setTimeout(delayTypingFn, timeWordTypingDelay);
   } else {
-    setTimeout(type, 80);
+    delayTypingFn();
   }
-})();
+}
